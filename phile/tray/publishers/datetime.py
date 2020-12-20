7b5cad56d787d7c51a71d269410a931043f8fc0c
Boni Lindsley <boni.lindsley@gmail.com>
2020-12-20 16:55:20 +0000

    refactor: remove tray datetime triggers
    Same as commit 36a8b28 for different tray files.

--- phile/tray/publishers/datetime.py
+++ phile/tray/publishers/datetime.py
@@ -3,62 +3,48 @@
 # Standard library.
 import asyncio
 import contextlib
+import dataclasses
 import datetime
-import functools
-import pathlib
 import sys
-import types
 import typing
-
-# External dependencies.
-import watchdog.observers  # type: ignore[import]
 
 # Internal packages.
 import phile.configuration
 import phile.tray
-import phile.trigger
-import phile.watchdog_extras
 
 
-class _TrayFiles(contextlib.AbstractContextManager):
-    """
-    Responsible for managing tray files for datetime.
+@dataclasses.dataclass
+class UpdateLoop:
 
-    Use :meth:`update` to write a specific time into them.
-    Use :meth:`unlink` to remove all the files.
-    Use as a context manager to unlink them on close.
-    """
+    configuration: dataclasses.InitVar[phile.configuration.Configuration]
+    prefix: dataclasses.InitVar[str] = '90-phile-tray-datetime-'
 
-    def __init__(
-        self, *args, configuration: phile.configuration.Configuration,
-        **kwargs
+    def __post_init__(
+        self, configuration: phile.configuration.Configuration,
+        prefix: str
     ) -> None:
-        # See: https://github.com/python/mypy/issues/4001
-        super().__init__(*args, **kwargs)  # type: ignore[call-arg]
         File = phile.tray.File
         self.files = tuple(
             File.from_path_stem(
-                configuration=configuration,
-                path_stem='90-phile-tray-datetime-' + suffix
+                configuration=configuration, path_stem=prefix + suffix
             ) for suffix in (
                 '1-year', '2-month', '3-day', '4-weekday', '5-hour',
                 '6-minute'
             )
         )
 
-    def __exit__(
-        self, exc_type: typing.Optional[typing.Type[BaseException]],
-        exc_value: typing.Optional[BaseException],
-        traceback: typing.Optional[types.TracebackType]
-    ) -> typing.Optional[bool]:
-        self.unlink()
-        return None
-
-    def unlink(self) -> None:
+    def close(self) -> None:
         for file in self.files:
             file.path.unlink(missing_ok=True)
 
-    def update(self, now: datetime.datetime) -> None:
+    async def run(self) -> None:
+        with contextlib.closing(self):
+            while True:
+                timeout = self.update().total_seconds()
+                await asyncio.sleep(timeout)
+
+    def update(self) -> datetime.timedelta:
+        now = datetime.datetime.now()
         datetime_values = (
             now.strftime(' %Y'), now.strftime('-%m'),
             now.strftime('-%d'), now.strftime('w%w'),
@@ -66,108 +52,18 @@ class _TrayFiles(contextlib.AbstractContextManager):
         )
         for value, file in zip(datetime_values, self.files):
             file.text_icon = value
+        for file in self.files:
             file.save()
-
-
-@contextlib.contextmanager
-def _prepare(
-    configuration: phile.configuration.Configuration,
-    watching_observer: watchdog.observers.Observer,
-    trigger_directory: pathlib.Path
-) -> typing.Iterator:
-    with contextlib.ExitStack() as exit_stack:
-        close_event = asyncio.Event()
-        hide_event = asyncio.Event()
-        show_event = asyncio.Event()
-        entry_point = exit_stack.enter_context(
-            phile.trigger.EntryPoint(
-                callback_map={
-                    'close': close_event.set,
-                    'hide': hide_event.set,
-                    'show': show_event.set,
-                },
-                configuration=configuration,
-                trigger_directory=trigger_directory,
-            )
+        return datetime.timedelta(
+            seconds=60 - now.second + 1 - now.microsecond / 1_000_000
         )
-        exit_stack.enter_context(
-            phile.watchdog_extras.Scheduler(
-                path_filter=entry_point.check_path,
-                path_handler=functools.partial(
-                    asyncio.get_running_loop().call_soon_threadsafe,
-                    entry_point.activate_trigger
-                ),
-                watched_path=entry_point.trigger_directory,
-                watching_observer=watching_observer,
-            )
-        )
-        # Triggers have to be added after scheduling
-        # so that their activations will be detected.
-        entry_point.add_trigger('close')
-        entry_point.add_trigger('hide')
-        yield close_event, hide_event, show_event, entry_point
-
-
-async def run(
-    configuration: phile.configuration.Configuration,
-    watching_observer: watchdog.observers.Observer,
-    trigger_directory: pathlib.
-    Path = (pathlib.Path('phile-tray-datetime'))
-) -> None:
-    with _prepare(
-        configuration, watching_observer, trigger_directory
-    ) as (close_event, hide_event, show_event, entry_point), _TrayFiles(
-        configuration=configuration
-    ) as tray_files:
-        hide_task = asyncio.create_task(hide_event.wait())
-        close_task = asyncio.create_task(close_event.wait())
-        show_task = asyncio.create_task(show_event.wait())
-        done_tasks: typing.Set[asyncio.Future] = set()
-        while close_task not in done_tasks:
-            timeout: typing.Optional[float]
-            if show_task in done_tasks:
-                await show_task
-                show_event.clear()
-                show_task = asyncio.create_task(show_event.wait())
-                entry_point.remove_trigger('show')
-                entry_point.add_trigger('hide')
-            if hide_task in done_tasks:
-                await hide_task
-                hide_event.clear()
-                hide_task = asyncio.create_task(hide_event.wait())
-                entry_point.remove_trigger('hide')
-                entry_point.add_trigger('show')
-                tray_files.unlink()
-                timeout = None
-            else:
-                now = datetime.datetime.now()
-                tray_files.update(now)
-                timeout = (
-                    60 - now.second + 1 - now.microsecond / 1_000_000
-                )
-            done_tasks, _pending_tasks = await asyncio.wait(
-                {close_task, hide_task, show_task},
-                timeout=timeout,
-                return_when=asyncio.FIRST_COMPLETED
-            )
-        gather_task = asyncio.gather(
-            close_task, hide_task, show_task, return_exceptions=True
-        )
-        gather_task.cancel()
-        with contextlib.suppress(asyncio.CancelledError):
-            await gather_task
 
 
 def main(argv: typing.List[str] = sys.argv) -> int:  # pragma: no cover
     configuration = phile.configuration.Configuration()
-    watching_observer = phile.watchdog_extras.Observer()
-    watching_observer.start()
-    asyncio.run(
-        run(
-            configuration=configuration,
-            watching_observer=watching_observer
-        )
-    )
+    with contextlib.suppress(KeyboardInterrupt):
+        run = UpdateLoop(configuration=configuration).run()
+        asyncio.run(run)
     return 0
 
 
--- test_phile/test_tray/test_publishers/test_datetime.py
+++ test_phile/test_tray/test_publishers/test_datetime.py
@@ -31,7 +31,7 @@ import test_phile.threaded_mock
 wait_time = datetime.timedelta(seconds=2)
 
 
-class TestTrayFiles(unittest.TestCase):
+class TestUpdateLoop(unittest.TestCase):
 
     def set_up_configuration(self) -> None:
         user_state_directory = tempfile.TemporaryDirectory()
@@ -39,15 +39,6 @@ class TestTrayFiles(unittest.TestCase):
         self.configuration = phile.configuration.Configuration(
             user_state_directory=pathlib.Path(user_state_directory.name)
         )
-
-    def setUp(self) -> None:
-        self.set_up_configuration()
-        self.tray_files = (
-            phile.tray.publishers.datetime._TrayFiles(
-                configuration=self.configuration
-            )
-        )
-        self.now = datetime.datetime(2222, 11, 1, 00, 59)
         tray_directory = self.configuration.tray_directory
         self.expected_tray_files = (
             tray_directory / '90-phile-tray-datetime-1-year.tray',
@@ -57,8 +48,34 @@ class TestTrayFiles(unittest.TestCase):
             tray_directory / '90-phile-tray-datetime-5-hour.tray',
             tray_directory / '90-phile-tray-datetime-6-minute.tray',
         )
+
+    def set_up_datetime_now(self) -> None:
+        # Cannot patch method of a built-in type.
+        # So we wrap it to make it possible.
+        patch = unittest.mock.patch(
+            'datetime.datetime', wraps=datetime.datetime
+        )
+        patch.start()
+        self.addCleanup(patch.stop)
+        # The actual mocking of `now` value.
+        patch = unittest.mock.patch.object(
+            datetime.datetime,
+            'now',
+            return_value=datetime.datetime(2222, 11, 1, 00, 59)
+        )
+        patch.start()
+        self.addCleanup(patch.stop)
         self.expected_text_icons = (
             ' 2222', '-11', '-01', 'w5', ' 00', ':59'
+        )
+
+    def setUp(self) -> None:
+        self.set_up_configuration()
+        self.set_up_datetime_now()
+        self.tray_files = (
+            phile.tray.publishers.datetime.UpdateLoop(
+                configuration=self.configuration
+            )
         )
 
     def assert_files_updated(self) -> None:
@@ -73,156 +90,32 @@ class TestTrayFiles(unittest.TestCase):
         for path in self.expected_tray_files:
             self.assertTrue(not path.exists())
 
-    def test_update(self) -> None:
-        self.tray_files.update(self.now)
+    def test_update_writes_tray_files(self) -> None:
+        self.tray_files.update()
         self.assert_files_updated()
 
-    def test_unlink(self) -> None:
-        self.tray_files.update(self.now)
-        self.tray_files.unlink()
-        self.assert_files_missing()
-
-    def test_context_manager(self) -> None:
-        with self.tray_files as files:
-            files.update(self.now)
-            self.assert_files_updated()
-        self.assert_files_missing()
-
-
-class TestRun(unittest.TestCase):
-
-    def set_up_configuration(self) -> None:
-        user_state_directory = tempfile.TemporaryDirectory()
-        self.addCleanup(user_state_directory.cleanup)
-        self.configuration = phile.configuration.Configuration(
-            user_state_directory=pathlib.Path(user_state_directory.name)
-        )
-
-    def set_up_observer(self) -> None:
-        self.observer = phile.watchdog_extras.Observer()
-        self.addCleanup(self.observer.stop)
-        self.observer.start()
-
-    def set_up_tray_event_dispatcher(self) -> None:
-        self.dispatcher = dispatcher = (
-            test_phile.threaded_mock.ThreadedMock()
-        )
-        tray_directory = self.configuration.tray_directory
-        tray_directory.mkdir(exist_ok=True)
-        watch = self.observer.add_handler(
-            event_handler=dispatcher, path=tray_directory
-        )
-        self.addCleanup(
-            self.observer.remove_handler,
-            event_handler=dispatcher,
-            watch=watch
-        )
-        self.expected_tray_files = (
-            tray_directory / '90-phile-tray-datetime-1-year.tray',
-            tray_directory / '90-phile-tray-datetime-2-month.tray',
-            tray_directory / '90-phile-tray-datetime-3-day.tray',
-            tray_directory / '90-phile-tray-datetime-4-weekday.tray',
-            tray_directory / '90-phile-tray-datetime-5-hour.tray',
-            tray_directory / '90-phile-tray-datetime-6-minute.tray',
-        )
-
-    def assert_files_created(self) -> None:
-        for file in self.expected_tray_files:
-            self.dispatcher.dispatch.assert_called_with_soon(
-                watchdog.events.FileCreatedEvent(str(file))
-            )
-
-    def assert_files_deleted(self) -> None:
-        for file in self.expected_tray_files:
-            self.dispatcher.dispatch.assert_called_with_soon(
-                watchdog.events.FileDeletedEvent(str(file))
-            )
-
-    def set_up_worker_thread(self) -> None:
-        self.run_stopped = threading.Event()
-        self.run_started = threading.Event()
-        self.worker_thread = threading.Thread(
-            target=asyncio.run, args=(self.run_start(), ), daemon=True
-        )
-        self.addCleanup(self.tear_down_worker_thread)
-        self.worker_thread.start()
-        self.assertTrue(
-            self.run_started.wait(timeout=wait_time.total_seconds())
-        )
-
-    async def run_start(self) -> None:
-        running_loop = self.running_loop = asyncio.get_running_loop()
-        main_task = self.main_task = running_loop.create_task(
-            phile.tray.publishers.datetime.run(
-                configuration=self.configuration,
-                watching_observer=self.observer
-            )
-        )
-        # Callbacks are called in schedule order.
-        # So `run` will be polled once before `started` event is set.
-        running_loop.call_soon(self.run_started.set)
-        with contextlib.suppress(asyncio.CancelledError):
-            await main_task
-        self.run_stopped.set()
-
-    def tear_down_worker_thread(self) -> None:
-        with contextlib.suppress(RuntimeError):
-            self.running_loop.call_soon_threadsafe(self.main_task.cancel)
-        self.run_stopped.wait(timeout=wait_time.total_seconds())
-
-    def set_up_new_entry_point(self) -> None:
-        self.entry_point = entry_point = phile.trigger.EntryPoint(
-            configuration=self.configuration,
-            trigger_directory=pathlib.Path('phile-tray-datetime')
-        )
-        self.trigger_directory = entry_point.trigger_directory
-
-    def activate_trigger(self, trigger_name: str) -> None:
-        self.entry_point.get_trigger_path(trigger_name).unlink()
-
-    def setUp(self) -> None:
-        self.set_up_configuration()
-        self.set_up_observer()
-        self.set_up_tray_event_dispatcher()
-        self.set_up_worker_thread()
-        self.set_up_new_entry_point()
-
-    def test_initialises_monitored_directories_and_files(self) -> None:
-        entry_point = self.entry_point
-        self.assertTrue(self.configuration.tray_directory.is_dir())
-        self.assertTrue(self.trigger_directory.is_dir())
-        self.assertTrue(entry_point.get_trigger_path('close').is_file())
-        self.assertTrue(entry_point.get_trigger_path('hide').is_file())
-        self.assertTrue(
-            not entry_point.get_trigger_path('show').exists()
-        )
-        self.assertTrue(not self.run_stopped.is_set())
-
-    def test_initialisation_creates_tray_files(self) -> None:
-        """
-        Ensure tray files are created.
-
-        Since the files are already created or updated with values
-        by delegating to `_TrayFiles`,
-        assume that the file contents are created properly,
-        and only check for the file existence.
-        """
-        self.assert_files_created()
-
     def test_close_removes_tray_files(self) -> None:
-        self.activate_trigger('close')
-        self.assert_files_deleted()
+        self.tray_files.update()
+        self.tray_files.close()
+        self.assert_files_missing()
 
-    def test_hide_removes_tray_files(self) -> None:
-        self.activate_trigger('hide')
-        self.assert_files_deleted()
+    def test_run_can_be_cancelled(self) -> None:
+        with unittest.mock.patch(
+            'asyncio.sleep',
+            side_effect=[unittest.mock.DEFAULT, asyncio.CancelledError]
+        ), self.assertRaises(asyncio.CancelledError):
+            asyncio.run(self.tray_files.run())
 
-    def test_show_recreates_tray_files(self) -> None:
-        self.activate_trigger('hide')
-        self.assert_files_deleted()
-        self.dispatcher.dispatch.reset_mock()
-        self.activate_trigger('show')
-        self.assert_files_created()
+    def test_run_calls_update(self) -> None:
+        tray_files = self.tray_files
+        with unittest.mock.patch(
+            'asyncio.sleep',
+            side_effect=[unittest.mock.DEFAULT, asyncio.CancelledError]
+        ), unittest.mock.patch.object(
+            tray_files, 'update', wraps=tray_files.update
+        ) as update_mock, self.assertRaises(asyncio.CancelledError):
+            asyncio.run(tray_files.run())
+            update_mock.assert_called_once_with()
 
 
 if __name__ == '__main__':
