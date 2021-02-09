9d7fe1f15ff91d167954f5daa071141cdeed9b50
Boni Lindsley <boni.lindsley@gmail.com>
2021-02-09 11:06:50 +0000

    feat: add CPU tray icon publisher

--- phile/tray/publishers/__init__.py
+++ phile/tray/publishers/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 .. automodule:: phile.tray.publishers.battery
+.. automodule:: phile.tray.publishers.cpu
 .. automodule:: phile.tray.publishers.datetime
 .. automodule:: phile.tray.publishers.imap_idle
 .. automodule:: phile.tray.publishers.memory
