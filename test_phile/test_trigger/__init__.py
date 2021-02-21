f92e488dc405a20369aebacb50c504db62a1b551
Boni Lindsley <boni.lindsley@gmail.com>
2021-02-21 01:15:24 +0000

    feat: add CLI for trigger

---
+++ test_phile/test_trigger/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+.. automodule:: test_phile.test_trigger.test_cli
+.. automodule:: test_phile.test_trigger.test_init
+.. automodule:: test_phile.test_trigger.test_tkinter
+"""
