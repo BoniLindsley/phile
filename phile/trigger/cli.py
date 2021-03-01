e752f9f86d63c88f6e48b8524e720e66d12150a4
Boni Lindsley <boni.lindsley@gmail.com>
2021-03-01 16:51:00 +0000

    feat: add class to list trigger in cmd

--- phile/trigger/cli.py
+++ phile/trigger/cli.py
@@ -10,8 +10,6 @@ import asyncio
 import collections.abc
 import cmd
 import contextlib
-import pathlib
-import queue
 import shlex
 import sys
 import typing
@@ -22,179 +20,111 @@ import watchdog.observers
 # Internal packages.
 import phile
 import phile.notify
+import phile.trigger.watchdog
 import phile.watchdog
-
-_Self = typing.TypeVar('_Self')
-_T = typing.TypeVar('_T')
-
-
-class IterableSimpleQueue(queue.SimpleQueue[_T]):
-
-    def __iter__(self: _Self) -> _Self:
-        return self
-
-    def __next__(self) -> _T:
-        try:
-            return self.get_nowait()
-        except queue.Empty:
-            raise StopIteration
-
-
-class Cache:
-
-    def __init__(
-        self, *args: typing.Any, trigger_root: pathlib.Path,
-        trigger_suffix: str, **kwargs: typing.Any
-    ) -> None:
-        # See: https://github.com/python/mypy/issues/4001
-        super().__init__(*args, **kwargs)  # type: ignore[call-arg]
-        self.trigger_root = trigger_root
-        self.trigger_suffix = trigger_suffix
-        self.available_triggers = set[pathlib.Path]()
-        self.expired_triggers = IterableSimpleQueue[pathlib.Path]()
-        self.known_triggers = dict[int, pathlib.Path]()
-        self.trigger_ids = dict[pathlib.Path, int]()
-        self.next_new_id = 1
-
-    def refresh(self) -> None:
-        self.purge()
-        put = self.expired_triggers.put
-        for trigger in sorted(
-            self.trigger_root.rglob('*' + self.trigger_suffix)
-        ):
-            put(trigger)
-        self.update_expired()
-
-    def execute(
-        self, trigger_ids: collections.abc.Iterable[int]
-    ) -> None:
-        triggers = [
-            self.known_triggers[trigger_id] for trigger_id in trigger_ids
-        ]
-        for trigger in triggers:
-            trigger.unlink(missing_ok=True)
-
-    def update_expired(self) -> None:
-        expired_triggers = list(self.expired_triggers)
-        self.assign_ids(expired_triggers)
-        self.update_availability(expired_triggers)
-
-    def update_availability(
-        self, triggers: collections.abc.Iterable[pathlib.Path]
-    ) -> None:
-        available_triggers = self.available_triggers
-        available_triggers.difference_update(triggers)
-        available_triggers.update(
-            trigger for trigger in triggers if trigger.is_file()
-        )
-
-    def assign_ids(
-        self, triggers: collections.abc.Iterable[pathlib.Path]
-    ) -> None:
-        trigger_ids = self.trigger_ids
-        known_triggers = self.known_triggers
-        for trigger in triggers:
-            new_id = self.next_new_id
-            trigger_id = trigger_ids.setdefault(trigger, new_id)
-            if trigger_id == new_id:
-                self.next_new_id += 1
-                known_triggers[new_id] = trigger
-
-    def purge(self) -> None:
-        self.available_triggers.clear()
-        self.known_triggers.clear()
-        self.trigger_ids.clear()
-        self.next_new_id = 1
-
-    def to_name(self, path: pathlib.Path) -> str:
-        assert self.is_trigger(path)
-        return str(path.relative_to(self.trigger_root))
-
-    def is_trigger(self, path: pathlib.Path) -> bool:
-        return path.is_relative_to(
-            self.trigger_root
-        ) and path.suffix == self.trigger_suffix
 
 
 class Prompt(cmd.Cmd):
 
     def __init__(
-        self, *args: typing.Any, cache: Cache, **kwargs: typing.Any
+        self, *args: typing.Any, capabilities: phile.Capabilities,
+        **kwargs: typing.Any
     ) -> None:
         super().__init__(*args, **kwargs)
-        self.cache = cache
+        self._registry = capabilities[phile.trigger.Registry]
+        self.known_triggers: list[str] = []
+        self.trigger_ids: dict[str, int] = {}
 
     def do_EOF(self, arg: str) -> typing.Literal[True]:
+        del arg
         return True
 
-    def do_refresh(self, arg: str) -> None:
-        self.cache.refresh()
+    def do_reset(self, arg: str) -> None:
+        del arg
+        self.known_triggers.clear()
+        self.trigger_ids.clear()
+        self.do_list('')
 
     def do_exe(self, arg: str) -> None:
         self.do_execute(arg)
 
     def do_execute(self, arg: str) -> None:
         argv = shlex.split(arg)
-        try:
-            trigger_ids = [int(entry) for entry in argv]
-        except ValueError as error:
-            self.stdout.write(
-                'Unable to parse trigger id. {error}'.format(
-                    error=error
+        trigger_ids: list[int] = []
+        for entry in argv:
+            try:
+                trigger_id = int(entry)
+            except ValueError as error:
+                self.stdout.write(
+                    'Unable to parse given trigger: {entry}\n'.format(
+                        entry=entry
+                    )
                 )
+                return
+            trigger_ids.append(trigger_id)
+        known_trigger_count = len(self.known_triggers)
+        for trigger_id in trigger_ids:
+            if trigger_id >= known_trigger_count:
+                self.stdout.write(
+                    'Unknown_trigger ID {trigger_id}.\n'.format(
+                        trigger_id=trigger_id
+                    )
+                )
+                return
+        failed_trigger_count = 0
+        for trigger_id in trigger_ids:
+            trigger_name = self.known_triggers[trigger_id]
+            try:
+                self._registry.activate(trigger_name)
+            except (
+                phile.trigger.Registry.NotBound,
+                phile.trigger.Registry.NotShown,
+            ):
+                failed_trigger_count += 1
+                self.stdout.write(
+                    'Failed to activate trigger'
+                    ' {trigger_id} {trigger_name}\n'.format(
+                        trigger_id=trigger_id,
+                        trigger_name=trigger_name,
+                    )
+                )
+        self.stdout.write(
+            'Activated {count} triggers.\n'.format(
+                count=len(trigger_ids) - failed_trigger_count
             )
-            return
-        self.cache.execute(trigger_ids)
+        )
 
     def do_list(self, arg: str) -> None:
-        cache = self.cache
-        to_name = cache.to_name
-        write = self.stdout.write
-        for trigger_id, path in cache.known_triggers.items():
-            if path in cache.available_triggers:
-                write(
-                    '{trigger_id} {name}\n'.format(
-                        trigger_id=trigger_id, name=to_name(path)
+        del arg
+        visible_triggers = self._registry.visible_triggers.copy()
+        for name in sorted(visible_triggers):
+            self._assign_id(name)
+        self.stdout.write(
+            'Listing IDs of {count} available triggers.\n'.format(
+                count=len(visible_triggers)
+            )
+        )
+        for trigger_id, name in enumerate(self.known_triggers):
+            if name in visible_triggers:
+                self.stdout.write(
+                    'Trigger {trigger_id} is {name}\n'.format(
+                        trigger_id=trigger_id, name=name
                     )
                 )
 
-    def do_list_all(self, arg: str) -> None:
-        cache = self.cache
-        to_name = cache.to_name
-        write = self.stdout.write
-        for trigger_id, path in cache.known_triggers.items():
-            write(
-                '{trigger_id} {name}\n'.format(
-                    trigger_id=trigger_id, name=to_name(path)
-                )
-            )
-
-    def onecmd(self, line: str) -> bool:
-        if line != 'EOF':
-            self.cache.update_expired()
-        return super().onecmd(line)
+    def _assign_id(self, trigger: str) -> None:
+        new_id = len(self.known_triggers)
+        trigger_id = self.trigger_ids.setdefault(trigger, new_id)
+        if trigger_id == new_id:
+            self.known_triggers.append(trigger)
+            assert self.known_triggers.index(trigger) == trigger_id
 
 
 async def run(
     capabilities: phile.Capabilities
 ) -> int:  # pragma: no cover
-    configuration = capabilities[phile.Configuration]
-    observer = capabilities[watchdog.observers.api.BaseObserver]
-    trigger_root = configuration.trigger_root
-    trigger_suffix = configuration.trigger_suffix
-    cache = Cache(
-        trigger_root=trigger_root, trigger_suffix=trigger_suffix
-    )
-    prompt = Prompt(cache=cache)
-    with phile.watchdog.Scheduler(
-        path_filter=cache.is_trigger,
-        path_handler=cache.expired_triggers.put,
-        watch_recursive=True,
-        watched_path=trigger_root,
-        watching_observer=observer,
-    ):
-        cache.refresh()
-        await asyncio.to_thread(prompt.cmdloop)
+    prompt = Prompt(capabilities=capabilities)
+    await asyncio.to_thread(prompt.cmdloop)
     return 0
 
 
@@ -202,9 +132,11 @@ async def async_main(argv: list[str]) -> int:  # pragma: no cover
     del argv
     capabilities = phile.Capabilities()
     capabilities.set(phile.Configuration())
+    capabilities.set(phile.trigger.Registry())
     async with phile.watchdog.observers.async_open() as observer:
         capabilities[watchdog.observers.api.BaseObserver] = observer
-        await run(capabilities=capabilities)
+        with phile.trigger.watchdog.Producer(capabilities=capabilities):
+            await run(capabilities=capabilities)
     return 0
 
 
--- test_phile/test_trigger/test_cli.py
+++ test_phile/test_trigger/test_cli.py
@@ -6,201 +6,117 @@ Test :mod:`phile.trigger.cli`
 """
 
 # Standard library.
-import argparse
 import io
-import pathlib
-import tempfile
 import unittest
+import unittest.mock
 
 # Internal packages.
 import phile
 import phile.trigger.cli
+from test_phile.test_init import UsesCapabilities
+from test_phile.test_trigger.test_init import UsesRegistry
 
 
-class TestIterableSimpleQueue(unittest.TestCase):
-    """Tests :func:`~phile.trigger.cli.IterableSimpleQueue`."""
-
-    def test_is_iteratable(self) -> None:
-        queue = phile.trigger.cli.IterableSimpleQueue[int]()
-        queue.put(8)
-        queue.put(5)
-        queue.put(3)
-        queue.put(2)
-        # Force use of `__iter__`.
-        self.assertEqual([number for number in queue], [8, 5, 3, 2])
-
-
-class TestCache(unittest.TestCase):
-    """Tests :func:`~phile.trigger.cli.Cache`."""
-
-    def setUp(self) -> None:
-        super().setUp()
-        trigger_directory = tempfile.TemporaryDirectory()
-        self.addCleanup(trigger_directory.cleanup)
-        self.trigger_root = pathlib.Path(trigger_directory.name)
-        self.trigger_suffix = '.t'
-        self.cache = phile.trigger.cli.Cache(
-            trigger_root=self.trigger_root,
-            trigger_suffix=self.trigger_suffix,
-        )
-        self.set_up_triggers()
-
-    def set_up_triggers(self) -> None:
-        put = self.cache.expired_triggers.put
-        trigger_root = self.cache.trigger_root
-        self.missing_trigger = trigger_root / 'missing.t'
-        put(self.missing_trigger)
-        self.new_trigger = trigger_root / 'new.t'
-        self.new_trigger.touch()
-        put(self.new_trigger)
-        self.cache.update_expired()
-
-    def test_refresh_resets_and_orders_id_by_filename(self) -> None:
-        self.missing_trigger.touch()
-        self.new_trigger.unlink(missing_ok=True)
-        self.extra_trigger = self.trigger_root / 'extra.t'
-        self.extra_trigger.touch()
-        self.cache.refresh()
-        self.assertEqual(
-            self.cache.available_triggers,
-            {self.extra_trigger, self.missing_trigger}
-        )
-        self.assertEqual(
-            self.cache.known_triggers, {
-                1: self.extra_trigger,
-                2: self.missing_trigger
-            }
-        )
-        self.assertEqual(
-            self.cache.trigger_ids, {
-                self.extra_trigger: 1,
-                self.missing_trigger: 2
-            }
-        )
-        self.assertEqual(self.cache.next_new_id, 3)
-
-    def test_refresh_empty_trigger_root(self) -> None:
-        self.new_trigger.unlink(missing_ok=True)
-        self.cache.refresh()
-        self.assertFalse(self.cache.available_triggers)
-        self.assertFalse(self.cache.known_triggers)
-        self.assertFalse(self.cache.trigger_ids)
-        self.assertEqual(self.cache.next_new_id, 1)
-
-    def test_execute_removes_trigger_file(self) -> None:
-        self.cache.execute([2])
-        self.assertFalse(self.new_trigger.exists())
-
-    def test_execute_empty_list_is_okay(self) -> None:
-        self.cache.execute([])
-
-    def test_update_expired_checks_expired_existence(self) -> None:
-        # Also checks that `expired_triggers` has ordered `put`.
-        self.assertEqual(
-            self.cache.available_triggers, {self.new_trigger}
-        )
-        self.assertEqual(
-            self.cache.known_triggers, {
-                1: self.missing_trigger,
-                2: self.new_trigger
-            }
-        )
-        self.assertEqual(
-            self.cache.trigger_ids, {
-                self.missing_trigger: 1,
-                self.new_trigger: 2
-            }
-        )
-        self.assertEqual(self.cache.next_new_id, 3)
-
-    def test_assign_ids_ignores_known_triggers(self) -> None:
-        self.cache.assign_ids([self.new_trigger])
-        self.assertEqual(self.cache.known_triggers[2], self.new_trigger)
-        self.assertEqual(self.cache.trigger_ids[self.new_trigger], 2)
-        self.assertEqual(self.cache.next_new_id, 3)
-
-    def test_purge_clears_all_data(self) -> None:
-        self.cache.purge()
-        self.assertFalse(self.cache.available_triggers)
-        self.assertFalse(self.cache.known_triggers)
-        self.assertFalse(self.cache.trigger_ids)
-        self.assertEqual(self.cache.next_new_id, 1)
-
-    def test_to_name_removes_trigger_root(self) -> None:
-        to_name = self.cache.to_name
-        trigger_root = self.trigger_root
-        self.assertEqual(to_name(trigger_root / 'a.t'), 'a.t')
-        self.assertEqual(to_name(trigger_root / 'd' / 'a.t'), 'd/a.t')
-
-    def test_is_trigger_checks_parents_and_suffix(self) -> None:
-        is_trigger = self.cache.is_trigger
-        trigger_root = self.trigger_root
-        self.assertTrue(is_trigger(trigger_root / 'a.t'))
-        self.assertTrue(is_trigger(trigger_root / 'd' / 'a.t'))
-        self.assertFalse(is_trigger(trigger_root / 'wrong.suffix'))
-        self.assertFalse(
-            is_trigger(pathlib.Path(str(trigger_root) + 'wrong_root'))
-        )
-
-
-class TestPrompt(unittest.TestCase):
+class TestPrompt(UsesRegistry, UsesCapabilities, unittest.TestCase):
     """Tests :func:`~phile.trigger.cli.Prompt`."""
 
     def setUp(self) -> None:
         super().setUp()
-        trigger_directory = tempfile.TemporaryDirectory()
-        self.addCleanup(trigger_directory.cleanup)
-        self.trigger_root = pathlib.Path(trigger_directory.name)
-        self.trigger_suffix = '.t'
-        self.cache = phile.trigger.cli.Cache(
-            trigger_root=self.trigger_root,
-            trigger_suffix=self.trigger_suffix,
-        )
         self.stdout = io.StringIO()
         self.prompt = phile.trigger.cli.Prompt(
-            cache=self.cache, stdout=self.stdout
+            capabilities=self.capabilities, stdout=self.stdout
         )
         self.prompt.use_rawinput = False
         self.set_up_triggers()
 
     def set_up_triggers(self) -> None:
-        self.missing_trigger = self.trigger_root / 'missing.t'
-        self.cache.expired_triggers.put(self.missing_trigger)
-        self.new_trigger = self.trigger_root / 'new.t'
-        self.new_trigger.touch()
-        self.cache.expired_triggers.put(self.new_trigger)
+        self.run_mock = unittest.mock.Mock()
+        self.trigger_registry.bind('run', self.run_mock)
+        self.trigger_registry.show('run')
+        self.clean_mock = unittest.mock.Mock()
+        self.trigger_registry.bind('clean', self.clean_mock)
+        self.trigger_registry.show('clean')
 
-    def test_do_EOF_stops_prompt(self) -> None:
+    def test_do_eof_stops_prompt(self) -> None:
         self.assertTrue(self.prompt.onecmd('EOF'))
 
-    def test_do_refresh(self) -> None:
-        self.assertFalse(self.prompt.onecmd('refresh'))
+    def test_do_reset_uses_id_from_one(self) -> None:
+        self.test_do_list_sorts_output_of_new_triggers()
+        self.trigger_registry.unbind('clean')
+        self.assertFalse(self.prompt.onecmd('reset'))
         self.assertEqual(
-            self.cache.available_triggers, {self.new_trigger}
+            self.stdout.getvalue(),
+            'Listing IDs of 2 available triggers.\n'
+            'Trigger 0 is clean\n'
+            'Trigger 1 is run\n'
+            'Listing IDs of 1 available triggers.\n'
+            'Trigger 0 is run\n'
         )
-        self.assertEqual(
-            self.cache.known_triggers, {1: self.new_trigger}
-        )
-        self.assertEqual(self.cache.trigger_ids, {self.new_trigger: 1})
-        self.assertEqual(self.cache.next_new_id, 2)
 
-    def test_do_exe(self) -> None:
-        self.assertFalse(self.prompt.onecmd('exe 2'))
-        self.assertFalse(self.new_trigger.exists())
+    def test_do_exe_activates_trigger(self) -> None:
+        self.assertFalse(self.prompt.onecmd('reset'))
+        self.assertFalse(self.prompt.onecmd('exe 1'))
+        self.run_mock.assert_called_with()
+
+    def test_do_execute_writes_to_stdout_on_trigger(self) -> None:
+        self.assertFalse(self.prompt.onecmd('reset'))
+        self.assertFalse(self.prompt.onecmd('exe 1 0'))
+        self.run_mock.assert_called_with()
+        self.assertEqual(
+            self.stdout.getvalue(),
+            'Listing IDs of 2 available triggers.\n'
+            'Trigger 0 is clean\n'
+            'Trigger 1 is run\n'
+            'Activated 2 triggers.\n'
+        )
 
     def test_do_execute_warns_wrong_argument(self) -> None:
         self.assertFalse(self.prompt.onecmd('exe a'))
         self.assertEqual(
-            self.stdout.getvalue(), 'Unable to parse trigger id.'
-            ' invalid literal for int() with base 10: \'a\''
+            self.stdout.getvalue(), 'Unable to parse given trigger: a\n'
         )
 
-    def test_do_list(self) -> None:
-        self.assertFalse(self.prompt.onecmd('list'))
-        self.assertEqual(self.stdout.getvalue(), '2 new.t\n')
-
-    def test_do_list_all(self) -> None:
-        self.assertFalse(self.prompt.onecmd('list_all'))
+    def test_do_execute_informs_unknown_triggers(self) -> None:
+        self.assertFalse(self.prompt.onecmd('exe 0'))
         self.assertEqual(
-            self.stdout.getvalue(), '1 missing.t\n2 new.t\n'
+            self.stdout.getvalue(), 'Unknown_trigger ID 0.\n'
         )
+
+    def test_do_execute_informs_hidden_triggers(self) -> None:
+        self.assertFalse(self.prompt.onecmd('reset'))
+        self.trigger_registry.unbind('clean')
+        self.assertFalse(self.prompt.onecmd('exe 0'))
+        self.assertEqual(
+            self.stdout.getvalue(),
+            'Listing IDs of 2 available triggers.\n'
+            'Trigger 0 is clean\n'
+            'Trigger 1 is run\n'
+            'Failed to activate trigger 0 clean\n'
+            'Activated 0 triggers.\n'
+        )
+
+    def test_do_list_sorts_output_of_new_triggers(self) -> None:
+        self.assertFalse(self.prompt.onecmd('list'))
+        self.assertEqual(
+            self.stdout.getvalue(),
+            'Listing IDs of 2 available triggers.\n'
+            'Trigger 0 is clean\n'
+            'Trigger 1 is run\n'
+        )
+
+    def test_do_list_hides_known_hidden_triggers(self) -> None:
+        self.assertFalse(self.prompt.onecmd('list'))
+        self.trigger_registry.hide('clean')
+        self.assertFalse(self.prompt.onecmd('list'))
+        self.assertEqual(
+            self.stdout.getvalue(),
+            'Listing IDs of 2 available triggers.\n'
+            'Trigger 0 is clean\n'
+            'Trigger 1 is run\n'
+            'Listing IDs of 1 available triggers.\n'
+            'Trigger 1 is run\n'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
