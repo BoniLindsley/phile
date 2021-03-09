28ef886d65d8c07d0ec6eec06196579f243a83c3
Boni Lindsley <boni.lindsley@gmail.com>
2021-03-09 00:06:26 +0000

    refactor: move phile.PySide2 to QtCore submodule
    This is to mimic PySide2 module's behaviour
    of having everything inside a submodule of the PySide2 module.

--- test_phile/test_PySide2/__init__.py
+++ test_phile/test_PySide2/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-.. automodule:: test_phile.test_PySide2.test_init
+.. automodule:: test_phile.test_PySide2.test_QtCore
 .. automodule:: test_phile.test_PySide2.test_QtGui
 .. automodule:: test_phile.test_PySide2.test_QtNetwork
 .. automodule:: test_phile.test_PySide2.test_QtWidgets
