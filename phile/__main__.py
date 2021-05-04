342cde09c38f93d4f7829d4a2f8e9fdaa976e044
Boni Lindsley <boni.lindsley@gmail.com>
2021-05-04 01:24:57 +0100

    refactor: main loop to use refactored submodules

--- phile/__main__.py
+++ phile/__main__.py
@@ -1,328 +1,57 @@
 #!/usr/bin/env python3
 
 # Standard library.
-import argparse
 import asyncio
-import collections.abc
 import contextlib
-import dataclasses
 import functools
 import sys
-import types
-import typing
-
-# External dependencies.
-import keyring
-import watchdog.observers
 
 # Internal packages.
 import phile
-import phile.asyncio
 import phile.capability
-import phile.capability.asyncio
-import phile.capability.pyside2
-import phile.capability.tmux
-import phile.tray.publishers.battery
-import phile.tray.publishers.cpu
-import phile.tray.publishers.datetime
-import phile.tray.publishers.imap_idle
-import phile.tray.publishers.memory
-import phile.tray.publishers.network
-import phile.tray.publishers.notify_monitor
-import phile.tray.publishers.update
-import phile.tray.tmux
-import phile.trigger
-import phile.trigger.cli
-import phile.watchdog
-
-NullaryCallable = typing.Callable[[], typing.Any]
-Launcher = typing.Callable[[phile.Capabilities], typing.Coroutine]
-LauncherEntry = tuple[Launcher, set[type]]
-
-_T_co = typing.TypeVar('_T_co')
-
-default_launchers: typing.Dict[str, LauncherEntry] = {
-    'imap-idle': (
-        phile.tray.publishers.imap_idle.run,
-        {
-            phile.Configuration,
-            keyring.backend.KeyringBackend,
-        },
-    ),
-    'tray-battery':
-        (phile.tray.publishers.battery.run, {
-            phile.Configuration,
-        }),
-    'tray-cpu': (phile.tray.publishers.cpu.run, {
-        phile.Configuration,
-    }),
-    'tray-datetime':
-        (phile.tray.publishers.datetime.run, {
-            phile.Configuration,
-        }),
-    'tray-memory':
-        (phile.tray.publishers.memory.run, {
-            phile.Configuration,
-        }),
-    'tray-network':
-        (phile.tray.publishers.network.run, {
-            phile.Configuration,
-        }),
-    'tray-notify': (
-        phile.tray.publishers.notify_monitor.run,
-        {
-            phile.Configuration,
-            watchdog.observers.api.BaseObserver,
-        },
-    ),
-    'tray-tmux': (
-        phile.tray.tmux.run,
-        {
-            phile.Configuration,
-            phile.tmux.control_mode.Client,
-            watchdog.observers.api.BaseObserver,
-        },
-    ),
-}
+import phile.cmd
+import phile.launcher
+import phile.launcher.cmd
 
 
-@dataclasses.dataclass
-class TaskRegistry:
-    """Keeps track of existing tasks and how to start new tasks."""
-
-    class MissingCapability(RuntimeError):
-        pass
-
-    capability_registry: phile.capability.Registry
-    launchers: types.MappingProxyType[
-        str, LauncherEntry] = types.MappingProxyType(default_launchers)
-
-    def __post_init__(self) -> None:
-        self.running_tasks: dict[str, asyncio.Task[typing.Any]] = {}
-        capability_set = set(self.capability_registry.keys())
-        self.usable_launcher_names = {
-            name
-            for name, launcher_entry in self.launchers.items()
-            if not launcher_entry[1].difference(capability_set)
-        }
-
-    def create_task(self, name: str) -> asyncio.Task[typing.Any]:
-        assert name not in self.running_tasks
-        launcher_entry = self.launchers[name]
-        if name not in self.usable_launcher_names:
-            required_launcher_capability_registry = launcher_entry[1]
-            missing_capability_registry = (
-                required_launcher_capability_registry.difference(
-                    self.capability_registry
-                )
-            )
-            raise TaskRegistry.MissingCapability(
-                f"Launcher {name} requires {missing_capability_registry}."
-            )
-        launcher = launcher_entry[0]
-        loop = phile.capability.asyncio.get_instance(
-            capability_registry=self.capability_registry,
-        )
-        self.running_tasks[name] = task = loop.create_task(
-            launcher(self.capability_registry), name=name
-        )
-        task.add_done_callback(self.on_task_done)
-        return task
-
-    def cancel_task(self, name: str) -> None:
-        assert name in self.running_tasks
-        self.running_tasks[name].cancel()
-
-    def on_task_done(self, task: asyncio.Task[typing.Any]) -> None:
-        self.running_tasks.pop(task.get_name())
-
-
-class TriggerProvider(phile.trigger.Provider):
-    """Provides triggers to start and stop tasks."""
-
-    __Self = typing.TypeVar('__Self', bound='TriggerProvider')
-
-    def __init__(
-        self,
-        *args: typing.Any,
-        capability_registry: phile.capability.Registry,
-        **kwargs: typing.Any,
-    ) -> None:
-        self._capability_registry = capability_registry
-        self.start_prefix = 'start-task_'
-        self.stop_prefix = 'stop-task_'
-        self.task_registry = TaskRegistry(
-            capability_registry=capability_registry
-        )
-        callbacks = self.get_triggers()
-        trigger_registry = capability_registry[phile.trigger.Registry]
-        super().__init__(
-            *args,
-            callback_map=callbacks,
-            registry=trigger_registry,
-            **kwargs,
-        )
-
-    def __enter__(self: __Self) -> __Self:
-        super().__enter__()
-        for task_name in self.task_registry.usable_launcher_names:
-            self.create_task(task_name)
-        return self
-
-    def get_triggers(self) -> dict[str, NullaryCallable]:
-        callbacks: dict[str, NullaryCallable] = {}
-        usable_launchers = {
-            name: launcher
-            for (name, launcher) in self.task_registry.launchers.items()
-            if name in self.task_registry.usable_launcher_names
-        }
-        loop = phile.capability.asyncio.get_instance(
-            capability_registry=self._capability_registry,
-        )
-        for task_name in usable_launchers:
-            callbacks[self.start_prefix + task_name] = functools.partial(
-                loop.call_soon_threadsafe,
-                self.create_task,
-                task_name,
-            )
-            callbacks[self.stop_prefix + task_name] = functools.partial(
-                self.task_registry.cancel_task, task_name
-            )
-        return callbacks
-
-    def create_task(self, task_name: str) -> asyncio.Task[typing.Any]:
-        self.hide(self.start_prefix + task_name)
-        self.show(self.stop_prefix + task_name)
-        task = self.task_registry.create_task(name=task_name)
-        task.add_done_callback(self.on_task_done)
-        return task
-
-    def on_task_done(self, task: asyncio.Task[typing.Any]) -> None:
-        task_name = task.get_name()
-        with contextlib.suppress(
-            ResourceWarning, phile.trigger.Provider.NotBound
-        ):
-            self.hide(self.stop_prefix + task_name)
-            self.show(self.start_prefix + task_name)
-
-
-class CleanUps:
-
-    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        # TODO[mypy issue 4001]: Remove type ignore.
-        super().__init__(*args, **kwargs)  # type: ignore[call-arg]
-        self.callbacks = set[NullaryCallable]()
-
-    @contextlib.contextmanager
-    def connect(
-        self, callback: NullaryCallable
-    ) -> collections.abc.Iterator[None]:
-        try:
-            self.callbacks.add(callback)
-            yield
-        finally:
-            self.callbacks.discard(callback)
-
-    def run(self) -> None:
-        for callback in self.callbacks.copy():
-            callback()
-
-
-def main(
-    argv: typing.Optional[list[str]] = None
+async def async_run(
+    capability_registry: phile.capability.Registry,
 ) -> int:  # pragma: no cover
-    capability_registry = phile.capability.Registry()
-    with contextlib.ExitStack() as stack:
-        stack.enter_context(contextlib.suppress(KeyboardInterrupt))
+    launcher_registry = capability_registry[phile.launcher.Registry]
+    launcher_registry.database.add(
+        launcher_name := 'phile.launcher.cmd',
+        phile.launcher.Descriptor(
+            after={'phile.launcher'},
+            binds_to={'phile.launcher'},
+            exec_start=[
+                functools.partial(
+                    phile.cmd.async_cmdloop_threaded_stdin,
+                    phile.launcher.cmd.Cmd(
+                        launcher_registry=launcher_registry,
+                    ),
+                ),
+            ],
+        )
+    )
+    state_machine = launcher_registry.state_machine
+    start = state_machine.start
+    await asyncio.gather(
+        *(start(name) for name in launcher_registry.database.type),
+        return_exceptions=True,
+    )
+    # pylint: disable=protected-access
+    running_tasks = state_machine._running_tasks
+    try:
+        cmd_task = running_tasks[launcher_name]
+    except KeyError:
+        return 1
+    await cmd_task
+    return 0
 
-        if argv is None:
-            argv = sys.argv
-        parser = argparse.ArgumentParser()
-        parser.add_argument(
-            '--gui', action=argparse.BooleanOptionalAction
-        )
-        args = parser.parse_args(argv[1:])
 
-        def stack_provide(
-            value: _T_co,
-            capability: typing.Optional[type[_T_co]] = None,
-        ) -> None:
-            stack.enter_context(
-                capability_registry.provide(value, capability)
-            )
-
-        stack_provide(phile.Configuration())
-        stack_provide(trigger_registry := phile.trigger.Registry())
-        stack_provide(clean_ups := CleanUps())
-        stack.enter_context(
-            quit_trigger_provider := phile.trigger.Provider(
-                callback_map={'quit': clean_ups.run},
-                registry=trigger_registry,
-            )
-        )
-        quit_trigger_provider.show_all()
-        stack_provide(
-            stack.enter_context(phile.watchdog.observers.open()),
-            watchdog.observers.api.BaseObserver,
-        )
-        stack_provide(
-            keyring.get_keyring(), keyring.backend.KeyringBackend
-        )
-        stack.enter_context(
-            phile.trigger.watchdog.View(
-                capabilities=capability_registry
-            )
-        )
-        stack.enter_context(
-            phile.capability.asyncio.provide(
-                capability_registry=capability_registry
-            )
-        )
-        loop = phile.capability.asyncio.get_instance(
-            capability_registry=capability_registry
-        )
-        stack.enter_context(
-            clean_ups.connect(
-                lambda: phile.capability.asyncio.
-                stop(capability_registry) if loop.is_running() else None
-            )
-        )
-        stack.enter_context(
-            phile.capability.tmux.provide_async_tmux_client(
-                capability_registry=capability_registry,
-            )
-        )
-        stack.enter_context(
-            TriggerProvider(capability_registry=capability_registry)
-        )
-        cli_task = loop.create_task(
-            phile.trigger.cli.async_run(
-                capability_registry=capability_registry
-            )
-        )
-        cli_task.add_done_callback(lambda _future: clean_ups.run())
-        use_pyside_2: bool
-        if args.gui is None:
-            use_pyside_2 = phile.capability.pyside2.is_available()
-        else:
-            use_pyside_2 = args.gui
-
-        if use_pyside_2:
-            stack.enter_context(
-                phile.capability.asyncio.start(capability_registry)
-            )
-            stack.enter_context(
-                phile.capability.pyside2.provide_qapplication_in(
-                    capability_registry=capability_registry,
-                )
-            )
-            stack.enter_context(
-                clean_ups.connect(phile.capability.pyside2.stop)
-            )
-            phile.capability.pyside2.run(capability_registry)
-            trigger_registry.activate_if_shown('quit')
-        else:
-            phile.capability.asyncio.run(capability_registry)
+def main() -> int:  # pragma: no cover
+    with contextlib.suppress(asyncio.CancelledError, KeyboardInterrupt):
+        return phile.main.run(async_run)
     return 0
 
 
