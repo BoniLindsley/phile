93fdf33585d2eaa3ed6cabca02a3528415aad61c
Boni Lindsley <boni.lindsley@gmail.com>
2021-05-27 13:50:03 +0100

    fix: tray GUI to stay on top without taking focus

--- phile/tray/pyside2_window.py
+++ phile/tray/pyside2_window.py
@@ -11,6 +11,7 @@ import contextlib
 import typing
 
 # External dependencies.
+import PySide2.QtCore
 import PySide2.QtWidgets
 
 # Internal modules.
@@ -19,12 +20,18 @@ import phile.pubsub_event
 import phile.tray
 
 
-class MainWindow(PySide2.QtWidgets.QMainWindow):
+class TrayDialog(PySide2.QtWidgets.QDialog):
 
     def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         super().__init__(*args, **kwargs)
-        self.tray_content = tray_content = PySide2.QtWidgets.QLabel()
-        self.setCentralWidget(tray_content)
+        self.setLayout(layout := PySide2.QtWidgets.QGridLayout(self))
+        self.tray_content = tray_content = PySide2.QtWidgets.QLabel(self)
+        layout.addWidget(tray_content)
+        self.setWindowFlag(PySide2.QtCore.Qt.WindowStaysOnTopHint)
+        self.setAttribute(PySide2.QtCore.Qt.WA_ShowWithoutActivating)
+        tray_content.setFocus(
+            PySide2.QtCore.Qt.FocusReason.OtherFocusReason
+        )
 
 
 async def run(
@@ -39,7 +46,7 @@ async def run(
     main_window_closed = asyncio.Event()
     # GUI functions must be called in GUI main thread.
     run_in_executor = loop.run_in_executor
-    main_window = await run_in_executor(pyside2_executor, MainWindow)
+    main_window = await run_in_executor(pyside2_executor, TrayDialog)
     try:
 
         def set_closed_event(
