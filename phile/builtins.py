e4b034b948ef128602bc8ddbcd4788f16f0954c9
Boni Lindsley <boni.lindsley@gmail.com>
2021-06-05 15:54:42 +0100

    feat[phill]: set dict item in a context
    That is, the item is popped when the context exits.
    This will be useful in launcher set up
    where launcher options are stored in dictionaries.

---
+++ phile/builtins.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Standard libraries.
+import contextlib
+import types
+import typing
+
+_Key = typing.TypeVar('_Key')
+_Value = typing.TypeVar('_Value')
+
+
+def provide_item(
+    target: dict[_Key, _Value],
+    key: _Key,
+    value: _Value,
+) -> contextlib.AbstractContextManager[None]:
+
+    class Popper(contextlib.AbstractContextManager[None]):
+
+        def __exit__(
+            self,
+            exc_type: typing.Optional[typing.Type[BaseException]],
+            exc_value: typing.Optional[BaseException],
+            traceback: typing.Optional[types.TracebackType],
+        ) -> None:
+            del exc_type
+            del exc_value
+            del traceback
+            with contextlib.suppress(KeyError):
+                del target[key]
+
+    target[key] = value
+    return Popper()
---
+++ test_phile/test_builtins.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+--------------------------
+Test :mod:`phile.builtins`
+--------------------------
+"""
+
+# Standard libraries.
+import sys
+import typing
+import unittest
+
+# Internal modules.
+import phile.builtins
+
+
+class TestProvideItem(unittest.TestCase):
+
+    def test_assigns_item(self) -> None:
+        target_map: dict[int, int] = {}
+        phile.builtins.provide_item(target_map, 0, 1)
+        self.assertEqual(target_map[0], 1)
+
+    def test_returns_context(self) -> None:
+        target_map: dict[int, int] = {}
+        with phile.builtins.provide_item(target_map, 0, 1):
+            self.assertIn(0, target_map)
+        self.assertNotIn(0, target_map)
